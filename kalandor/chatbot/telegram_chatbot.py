2d696877562c11e9ad17720f7d4de68fce1ee8ed
riotr <reveland92@gmail.com>
2018-12-24 15:00:19 +0100

    add logging to telegram chatbot

--- kalandor/chatbot/telegram_chatbot.py
+++ kalandor/chatbot/telegram_chatbot.py
@@ -1,19 +1,26 @@
+import logging
 import os
 import requests
 import json
 from kalandor.chatbot.chatbot import ChatBot
 
+logger = logging.getLogger(__name__)
+
 
 class TelegramChatBot(ChatBot):
 
     def get_message(self, request):
+        logger.debug('Telegram chatbot received request: %s',
+                     request.get_json())
         if request.method == "POST":
             update = request.get_json()
             if "message" in update:
                 if 'text' in update['message']:
                     message = {}
-                    message['user_id'] = update['message']['from']['id']
+                    message['user_id'] = update['message']['chat']['id']
                     message['text'] = update['message']['text']
+                    logger.info(
+                        'Telegram chatbot received message: %s', message)
                     return message
                 else:
                     return None
@@ -23,6 +30,7 @@ class TelegramChatBot(ChatBot):
             return None
 
     def send_message(self, user_id, answer):
+        logger.debug('Telegram chatbot send answer to %s: %s', user_id, answer)
         if 'text' in answer:
             data = {}
             data["chat_id"] = user_id
